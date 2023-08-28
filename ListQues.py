60e018bd1848d22a4150d3f9d14cf9c9b5942350
Tejas <tejaskothari03@gmail.com>
2023-08-28 18:04:31 +0530

    List questions & ref links

---
+++ ListQues.py
@@ -0,0 +1,11 @@
+"""
+Create a list having elements 1,2,3,4,5.
+Write function for following opeartions :
+1. Swap any 2 elements of which indexes are passed eg 0, 4 --> 5,2,3,4,1
+2. Maximum value of that list ---> 5
+3. Minimum value of that list --> 2
+4. Check if list is sorted in ascending order & accordingly return boolean value. --> True
+Ref link :- 
+- https://www.w3schools.com/python/python_lists.asp
+- https://www.geeksforgeeks.org/python-lists/
+"""
